1ac4c655010c623fce449b10168f541b0abf705d
Esraa Emad <72940480+esraa176@users.noreply.github.com>
2022-10-21 23:12:35 +0200

    Updated Adder
    Updated Adder to support multiple signals with different sizes.

--- DSPComponents/Algorithms/Adder.cs
+++ DSPComponents/Algorithms/Adder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +14,38 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            List<float> outputsignals = new List<float>();
-            for (int i = 0; i < InputSignals[0].Samples.Count; i++)
+            int maxSize = 0;
+            for (int i = 0; i < InputSignals.Count; i++)
             {
+                if (InputSignals[i].Samples.Count > maxSize)
+                    maxSize = InputSignals[i].Samples.Count;
+            }
 
-                outputsignals.Add(InputSignals[0].Samples[i] + InputSignals[1].Samples[i]);
+            List<float> outputsignals = new List<float>(maxSize);
+            for (int i = 0; i < maxSize; i++)
+            {
+                outputsignals.Add(0f);
+            }
 
+            for (int i = 0; i < InputSignals.Count; i++)
+            {
+                int size = InputSignals[i].Samples.Count;
+                for (int j = 0; j < size; j++)
+                {
+                    outputsignals[j] += InputSignals[i].Samples[j];
+                }
             }
             OutputSignal = new Signal(outputsignals, false);
+
+
+            //List<float> outputsignals = new List<float>();
+            //for (int i = 0; i < InputSignals[0].Samples.Count; i++)
+            //{
+
+            //    outputsignals.Add(InputSignals[0].Samples[i] + InputSignals[1].Samples[i]);
+
+            //}
+            //OutputSignal = new Signal(outputsignals, false);
 
         }
     }
