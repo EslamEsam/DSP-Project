293cc6554238f1235f0a516455fb7ef621a06bf5
Esraa Emad <72940480+esraa176@users.noreply.github.com>
2022-12-01 16:02:44 +0200

    Task 6 - No test passes

---
+++ DSPComponents/Algorithms/DCT.cs
@@ -0,0 +1,35 @@
+﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class DCT: Algorithm
+    {
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+        public override void Run()
+        {
+            List<float> outputSignal = new List<float>();
+            int N = InputSignal.Samples.Count;
+
+            float summation;
+            for(int k = 0; k < N; k++)
+            {
+                summation = 0;
+                for (int n = 0; n < N; n++)
+                {
+                    float cos = (float)Math.Cos((float)Math.PI/(4*(float)N) * (2*n-1) * (2*k-1));
+                    summation += InputSignal.Samples[n] * cos;
+                }
+                outputSignal.Add((float)Math.Sqrt(2/(float)N) * summation);
+            }
+
+            OutputSignal = new Signal(outputSignal, false);
+        }
+    }
+}
