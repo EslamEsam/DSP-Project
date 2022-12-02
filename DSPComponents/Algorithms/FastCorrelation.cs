b29d5d004e6aa80fd418a6d51c2a1a687b1c28d3
Esraa Emad <72940480+esraa176@users.noreply.github.com>
2022-12-03 00:38:28 +0200

    Task 6 - FastCorrelation Passes

--- DSPComponents/Algorithms/FastCorrelation.cs
+++ DSPComponents/Algorithms/FastCorrelation.cs
@@ -33,13 +33,11 @@ namespace DSPAlgorithms.Algorithms
             // Converting InputSignal1 to Fourier Transform
             DiscreteFourierTransform DFTSignal1 = new DiscreteFourierTransform();
             DFTSignal1.InputTimeDomainSignal = InputSignal1;
-            DFTSignal1.InputSamplingFrequency = 4;
             DFTSignal1.Run();
 
             // Converting InputSignal2 to Fourier Transform
             DiscreteFourierTransform DFTSignal2 = new DiscreteFourierTransform();
             DFTSignal2.InputTimeDomainSignal = InputSignal2;
-            DFTSignal2.InputSamplingFrequency = 4;
             DFTSignal2.Run();
 
             List<Complex> signal1 = new List<Complex>();
@@ -52,25 +50,37 @@ namespace DSPAlgorithms.Algorithms
                 phaseShift = DFTSignal1.OutputFreqDomainSignal.FrequenciesPhaseShifts[i];
                 real = A * (float)Math.Cos(phaseShift);
                 imaginary = A * (float)Math.Sin(phaseShift);
-                signal1.Add(new Complex(real, imaginary));
+                signal1.Add(new Complex(real, -1 * imaginary));   // X1*(K)
 
                 A = DFTSignal2.OutputFreqDomainSignal.FrequenciesAmplitudes[i];
                 phaseShift = DFTSignal2.OutputFreqDomainSignal.FrequenciesPhaseShifts[i];
                 real = A * (float)Math.Cos(phaseShift);
                 imaginary = A * (float)Math.Sin(phaseShift);
-                signal2.Add(new Complex(real, -1 * imaginary));   // X1*(K)
+                signal2.Add(new Complex(real, imaginary));
             }
 
             // X1*(K) . X2(K)
-            List<float> X = new List<float>();
+            List<Complex> X = new List<Complex>();
             for (int i = 0; i < N; i++)
             {
-                X.Add((float)(signal1[i].Real * signal2[i].Real) + (float)(signal1[i].Imaginary * signal2[i].Imaginary));
+                X.Add(Complex.Multiply(signal1[i], signal2[i]));
             }
 
             // FD-1 {X1*(K) . X2(K)}
+            Signal Idft = new Signal(new List<float>(), false);
+            Idft.FrequenciesAmplitudes = new List<float>();
+            Idft.FrequenciesPhaseShifts = new List<float>();
+            for (int i = 0; i < N; i++)
+            {
+                double realTmp = Math.Pow(X[i].Real, 2);
+                double imagTmp = Math.Pow(X[i].Imaginary, 2);
+                Idft.FrequenciesAmplitudes.Add((float)Math.Sqrt(realTmp + imagTmp));
+
+                float phaseShiftTmp = (float)Math.Atan2(X[i].Imaginary, X[i].Real);
+                Idft.FrequenciesPhaseShifts.Add(phaseShiftTmp);
+            }
             InverseDiscreteFourierTransform IDFT = new InverseDiscreteFourierTransform();
-            IDFT.InputFreqDomainSignal = new Signal(X, false);
+            IDFT.InputFreqDomainSignal = Idft;
             IDFT.Run();
 
             // 1/N . FD-1 {X1*(K) . X2(K)}  + Normalization
