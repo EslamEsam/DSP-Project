9e6eed8e2b7b1090e4d8808fedb8c87a0b1eb678
Eslam Esam <eslamesam3875@gmail.com>
2022-12-12 18:43:45 +0200

    putting new files

---
+++ DSPComponents/Algorithms/Sampling.cs
@@ -0,0 +1,27 @@
+﻿﻿using DSPAlgorithms.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPAlgorithms.Algorithms
+{
+    public class Sampling : Algorithm
+    {
+        public int L { get; set; } //upsampling factor
+        public int M { get; set; } //downsampling factor
+        public Signal InputSignal { get; set; }
+        public Signal OutputSignal { get; set; }
+
+
+
+
+        public override void Run()
+        {
+            // throw new NotImplementedException();
+
+        }
+    }
+
+}
